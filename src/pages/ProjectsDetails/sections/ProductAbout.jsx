bf418696e4326b728ffada8608c1b1132b996af1
Walid Mostafa <amadalioo980@gmail.com>
2026-01-12 14:10:55 +0200

    add location btn

--- src/pages/ProjectsDetails/sections/ProductAbout.jsx
+++ src/pages/ProjectsDetails/sections/ProductAbout.jsx
@@ -1,40 +1,53 @@
 import { Link } from "react-router-dom";
-import SectionTitle from "../../../components/common/SectionTitle";
 import { AiOutlineDownload } from "react-icons/ai";
 import { useTranslation } from "react-i18next";
+import { FaLocationDot } from "react-icons/fa6";
 
 const ProductAbout = ({ data }) => {
   const { t } = useTranslation();
 
   return (
     <section className="container sectionPadding">
-      <div>
-        <SectionTitle
-          wide
-          description={data?.name}
-          title={t("productAbout.title")}
-          subtitle={data?.description}
-          html
-        />
+      <div className="space-y-4">
+        {data?.name && <p className="text-myGold">{data?.name}</p>}
+
+        <h2 className="text-3xl lg:text-4xl font-bold capitalize">
+          {t("productAbout.title")}
+        </h2>
+
+        {data?.description && (
+          <div dangerouslySetInnerHTML={{ __html: data?.description }} />
+        )}
 
         <div className="flex flex-wrap gap-4 mt-4">
-          <Link
-            to={data?.general_profile_file}
-            target="_blank"
-            className="mainBtn"
-          >
-            {t("productAbout.generalProfile")}
-            <AiOutlineDownload />
-          </Link>
+          {data?.general_profile_file && (
+            <Link
+              to={data?.general_profile_file}
+              target="_blank"
+              className="mainBtn"
+            >
+              {t("productAbout.generalProfile")}
+              <AiOutlineDownload />
+            </Link>
+          )}
 
-          <Link
-            to={data?.residential_profile_file}
-            target="_blank"
-            className="mainBtn"
-          >
-            {t("productAbout.residentialProfile")}
-            <AiOutlineDownload />
-          </Link>
+          {data?.residential_profile_file && (
+            <Link
+              to={data?.residential_profile_file}
+              target="_blank"
+              className="mainBtn"
+            >
+              {t("productAbout.residentialProfile")}
+              <AiOutlineDownload />
+            </Link>
+          )}
+
+          {data?.location_link && (
+            <Link to={data?.location_link} target="_blank" className="mainBtn">
+              {t("productAbout.location_link")}
+              <FaLocationDot />
+            </Link>
+          )}
         </div>
       </div>
     </section>
